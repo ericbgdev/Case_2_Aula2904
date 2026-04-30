a0535b4cc1b4ba4d097c5bf484d0f8b1e12bf3d5
Eric <eric.gudera@sou.unifeob.edu.br>
2026-04-29 21:59:52 -0300

    Add to_dict method to Usuario class

--- model/usuario.py
+++ model/usuario.py
@@ -3,3 +3,6 @@ class Usuario:
         self.id = id
         self.nome = nome
         self.email = email
+
+    def to_dict(self) -> dict:
+        return {"id": self.id, "nome": self.nome, "email": self.email}
